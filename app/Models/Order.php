e777b72bf4c0050b8b9ce80830b9ac5d42f35aa7
Ramdani <ramdani@Ramdanis-MacBook-Air.local>
2023-02-28 00:03:46 +0800

    Init models scheme

---
+++ app/Models/Order.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'orderable_type',
+        'orderable_id',
+        'quantity',
+        'total_price',
+        'user_id',
+    ];
+    protected $dateFormat = 'U';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * Relation to orderable models.
+     */
+    public function orderable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Relation to User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
